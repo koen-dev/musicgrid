28e7d8229373dbe0c39dec3e1218bcd6cdb0715b
Koen028 <hendriks.k@ziggo.nl>
2017-10-14 18:48:08 +0200

    Init

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello World!</h1>,
+    document.getElementById('app')
+);
---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: path.resolve(__dirname, "src/App.jsx"),
+        vendor: ['react', 'react-dom']
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: '[name].js',
+        publicPath: '/dist/'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader?cacheDirectory',
+                    options: {
+                        presets: ['env', 'react']
+                    }
+                }
+            }
+        ]
+    }
+};
